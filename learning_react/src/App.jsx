6fa22c23fbc67816f0c07a77e9e5f53e6806ede4
sanjeevkumar-540 <sanjeevkumar200598@gmail.com>
2024-12-12 22:09:29 +0530

    Update App.jsx

--- learning_react/src/App.jsx
+++ learning_react/src/App.jsx
@@ -1,35 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import { Provider } from 'react-redux';
+import ProductList from './ProductList';
+import Cart from './CartItem';
+import store from './store';
+import './App.css';
+import AboutUs from './AboutUs';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [showProductList, setShowProductList] = useState(false);
+  const [showCart, setShowCart] = useState(false);
+
+  const handleGetStartedClick = () => {
+    setShowProductList(true);
+    setShowCart(false);
+  };
+
+  const handleViewCartClick = () => {
+    setShowCart(true);
+    setShowProductList(false);
+  };
+
+  const handleContinueShoppingClick = () => {
+    setShowCart(false);
+    setShowProductList(true);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <Provider store={store}>
+      <div className="app-container">
+        {!showProductList && !showCart && (
+          <div className="landing-page">
+            <div className="background-image"></div>
+            <div className="content">
+              <div className="landing_content">
+                <h1>Welcome To Paradise Nursery</h1>
+                <div className="divider"></div>
+                <p>Where Green Meets Serenity</p>
+                <button className="get-started-button" onClick={handleGetStartedClick}>
+                  Get Started
+                </button>
+              </div>
+              <div className="aboutus_container">
+                <AboutUs />
+              </div>
+            </div>
+          </div>
+        )}
+        {showProductList && !showCart && (
+          <ProductList onViewCartClick={handleViewCartClick} />
+        )}
+        {showCart && (
+          <Cart onContinueShopping={handleContinueShoppingClick} />
+        )}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </Provider>
+  );
 }
 
-export default App
+export default App;
